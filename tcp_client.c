5a3cbf460e2930903c80620434ce9bf44b8ae2ff
cheki <borna986@gmail.com>
2021-02-04 21:18:45 +0100

    update

--- tcp_client.c
+++ tcp_client.c
@@ -29,14 +29,13 @@ bool recv__file(int sockfd)
 {
     FILE *fp;
     char buff[MAX_BUFFER], recv_file[MAX_BUFFER], chr_to_str[1];
-    ssize_t bytes, send_bytes;
     char *ext, *fulllfpath, *ptrstrtolend, *strfilesize; // ext & ptrstrtolend don't use malloc??
     char *lfpath = "./recvfile";
-    int i, cnt, overall_bytes = 0;
-    ssize_t filesize = 0;
+    ssize_t bytes, send_bytes, filesize, overall_bytes = 0;
+    int i, cnt = 0;
 
     if(recv(sockfd, recv_file, MAX_BUFFER -1, 0) <= 0){
-        printf("Failed to recieve file metadata.\n");
+        perror("recv() metadata error");
         return false;
     }
 
@@ -44,12 +43,12 @@ bool recv__file(int sockfd)
     strfilesize = malloc((strlen(recv_file) +1) - (strlen(ext) +1));
 
     if(strfilesize == NULL){
-        perror("strfilesize malloc failed.");
+        perror("malloc() strfilesize error");
         return false;
     }
 
     // strcat operates on strings => chr_to_str as temp var
-    for(i=0;i<strlen(recv_file);i++)
+    for(i=0; i<strlen(recv_file); i++)
     {
         chr_to_str[0] = recv_file[i];
         if(isdigit(recv_file[i])){
@@ -61,7 +60,7 @@ bool recv__file(int sockfd)
     printf("Extension: %s\nFile size: %ld\n", ext, filesize);
 
     if(ext == NULL){
-        printf("No file extension\n");
+        printf("No file extension.\n");
         fulllfpath = malloc(strlen(lfpath) +1);
         strncpy(fulllfpath, lfpath, strlen(lfpath) +1);
     }
@@ -72,41 +71,37 @@ bool recv__file(int sockfd)
     }
 
     if(fulllfpath == NULL){
-        perror("fullpath malloc error.");
+        perror("malloc() fullpath error");
         return false;
     }
-
-    if(fulllfpath[strlen(fulllfpath) -1] == '\n')
+    if(fulllfpath[strlen(fulllfpath) -1] == '\n'){
         fulllfpath[strlen(fulllfpath) -1] = '\0';
-
+    }
     if((fp = fopen(fulllfpath, "wb")) == NULL){
-        fprintf(stderr, "Error fopen() wb\n");
+        fprintf(stderr, "error fopen() wb\n");
         return false;
     }
 
     printf("Path: %s\n", realpath(fulllfpath, NULL));
-
     while(1)
     {
         cnt++;
         bytes = 0;
         memset(buff, 0, sizeof(buff));
-
         if((bytes = recv(sockfd, buff, MAX_BUFFER -1, 0)) > 0)
         {
-            if(buff[bytes -1] == '\n')
+            if(buff[bytes -1] == '\n'){
                 buff[bytes -1] = '\0';
-
+            }
             overall_bytes += bytes;
-
-            printf("Iteration [%d] - Bytes [%ld]\n", cnt, bytes);
+            printf("Iteration [%d] - Bytes [%ld] - Overall Bytes [%ld]\n", cnt, bytes, overall_bytes);
             // close after spqcial sequence recieved => EOF
             if(strstr(buff, "--sf--end") != NULL){
                 printf("End of transmission..\n");
                 break;
             }
-            if (fwrite(buff, sizeof(char), bytes, fp) != bytes){
-                perror("Write File Error");
+            if(fwrite(buff, sizeof(char), bytes, fp) != bytes){
+                perror("fwrite() error");
                 break;
             }
         }
@@ -115,13 +110,12 @@ bool recv__file(int sockfd)
             break;
         }
         if(bytes == -1){
-            perror("recv error.");
+            perror("recv() error");
             return false;
         }
 
     }
-
-    printf("Recieved: [%d]b\n", overall_bytes);
+    printf("Recieved: [%ld]b\n", overall_bytes);
     free(fulllfpath);
     free(strfilesize);
     fclose(fp);
@@ -132,46 +126,42 @@ void *send_handler(void *args) // passing multiple args by pointer to struct
 { 
     thr_args *send_args = args;
     ssize_t bytes = 0;
-
     printf("Send handler start..\n");
-
     while(1)
     {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
-
         memset(send_args->send_msg, '\0', sizeof(send_args->send_msg));
         fgets(send_args->send_msg, sizeof(send_args->send_msg), stdin);
 
-        if(send_args->send_msg[strlen(send_args->send_msg) -1] == '\n')
+        if(send_args->send_msg[strlen(send_args->send_msg) -1] == '\n'){
             send_args->send_msg[strlen(send_args->send_msg) -1] = '\0';
-
+        }
         if(send_args->send_msg[0] == '\0'){
             printf("Enter msg..\n");
         }
-
-        else if(!strncmp(send_args->send_msg,"x",1))
-        {
+        else if(!strncmp(send_args->send_msg,"x",1)){
             printf("[!] Exit.\n");
             exit(0);
         }
-
         else
         {
             // check if client is connected
-            if(recv(send_args->sock,NULL,1, MSG_PEEK | MSG_DONTWAIT) == 0)
-            {
+            if(recv(send_args->sock,NULL,1, MSG_PEEK | MSG_DONTWAIT) == 0){
                 printf("Client disconnected.\n");
                 break;
             }
-
             if((bytes = send(send_args->sock, send_args->send_msg, strlen(send_args->send_msg), 0)) <= 0){
-                printf("Send error\n");
+                perror("send() error");
             }
-
             printf("%d-%02d-%02d %02d:%02d:%02d: Sent to server! \tbytes: %ld\n",
-                    tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
-                    tm.tm_min, tm.tm_sec, bytes
+                    tm.tm_year + 1900,
+                    tm.tm_mon + 1,
+                    tm.tm_mday, 
+                    tm.tm_hour,
+                    tm.tm_min,
+                    tm.tm_sec,
+                    bytes
             );
         }
     }
@@ -179,51 +169,52 @@ void *send_handler(void *args) // passing multiple args by pointer to struct
     pthread_exit(0);
 }
 
-void *recv_handler(void *args){
-
+void *recv_handler(void *args)
+{
     thr_args *recv_args = args;
     ssize_t bytes = 0;
-
     printf("Recv handler start..\n");
-
     while(1)
     {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
-
         // returns number of bytes received, or -1 if error
         bytes = recv(recv_args->sock, recv_args->buffer, sizeof(recv_args->buffer), 0);
-
-        if(bytes > 0){
-            if(recv_args->buffer[bytes -1] == '\n')
+        if(bytes > 0)
+        {
+            if(recv_args->buffer[bytes -1] == '\n'){
                 recv_args->buffer[bytes -1] = '\0';
-
+            }
             if(strncmp(recv_args->buffer,"-sf",3) == 0)
             {
                 printf("Recieving file..\n");
                 if(recv__file(recv_args->sock) == false){
-                    printf("recv file error..\n");
+                    perror("recv__file() error");
                     break;
                 }
                 memset(recv_args->buffer, 0, MAX_BUFFER);
                 continue;
             }
-
-            printf("%d-%02d-%02d %02d:%02d:%02d: Server : %s\n",tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, recv_args->buffer);
+            printf("%d-%02d-%02d %02d:%02d:%02d: Server : %s\n",
+                tm.tm_year + 1900,
+                tm.tm_mon + 1,
+                tm.tm_mday,
+                tm.tm_hour,
+                tm.tm_min,
+                tm.tm_sec,
+                recv_args->buffer
+            );
             memset(recv_args->buffer, 0, MAX_BUFFER);
         }
-
         else if(bytes == 0){
             printf("No data from server..\n");
             break;
         }
-
         else {
-            perror("recv error");
+            perror("recv() error");
             break;
         }
     }
-
     printf("Recv handler exited normaly..\n");
     pthread_exit(0);
 }
@@ -234,48 +225,38 @@ bool isValidIp(const char *ip){
     return result == 1;
 }
 
-int main(int argc, const char **argv)
-{
-    struct sockaddr_in serv_addr;  // dest
+int main(int argc, const char **argv) {
+    struct sockaddr_in serv_addr;  /* dest */
+    int i, conv_addr, port = 0;    /* converted addr */
     char input_addr [15];
-    int conv_addr = 0;  // converted addr
-    int port = 0;
-    int i;
 
     memset(&serv_addr, '\0', sizeof(serv_addr));
     memset(input_addr, '\0', sizeof(input_addr));
 
-    if(argc < 3)
-    {
+    if(argc < 3){
         printf("Enter ip/port.\nUsage: ./client <ip_addr> <port>\n");
         return 1;
     }
-
     if(!isValidIp(argv[1])){
         printf("Invalid ip.\n");
         return 1;
     }
-
     strncpy(input_addr, argv[1], strlen(argv[1]));
-
-    for(i=0; i<strlen(argv[2]); i++) {
-
-        if(!isdigit(argv[2][i])) {
+    for(i=0; i<strlen(argv[2]); i++)
+    {
+        if(!isdigit(argv[2][i])){
             printf("Invalid port.\n");
             return 1;
         }
     }
-
     if((port = atoi(argv[2])) > 65535 && port < 0){
         printf("Port number not in range.\n");
         return 1;
     }
-
     thr_args *args = malloc(sizeof *args);
-
     if((args->sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\nSocket creation error..\n");
-        return -1;
+        perror("socket() create error");
+        return 1;
     }
 
     serv_addr.sin_family      = AF_INET;
@@ -283,40 +264,33 @@ int main(int argc, const char **argv)
     serv_addr.sin_port        = htons(port);
 
     conv_addr = inet_pton(AF_INET, input_addr, &serv_addr.sin_addr);
-
     if(conv_addr <= 0) {
-        printf("\nInvalid address/Address not supported..\n");
-        return -1;
+        perror("invalid | not supported address.");
+        return 1;
     }
-
-
     // sockId, foreignAddr, addrLen
-    if (connect(args->sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
+    if(connect(args->sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect() failed.");
+        return 1;
     }
 
     printf("Connected to host [%s:%d]\n", input_addr, port);
-
     pthread_t send_handler_id;
     pthread_t recv_handler_id;
 
     if(pthread_create(&recv_handler_id, NULL, recv_handler, args) != 0){
-        printf("Error creating recv handler..");
+        perror("pthread_create() recv_handler error");
+        return 1;
     }
-    
     if(pthread_create(&send_handler_id, NULL, send_handler, args) != 0){
-        printf("Error creating send handler..");
+        perror("pthread_create() send_handler error");
+        return 1;
     }
-
     if(pthread_join(send_handler_id,NULL) == 0 
-     && pthread_join(recv_handler_id,NULL) == 0){
-        printf("Worker disconnected normaly..\n");
-    }
-    else {
-        printf("Worker disconnected with error..\n");
-    }
-
+    && pthread_join(recv_handler_id,NULL) == 0)
+    { printf("Worker disconnected normaly..\n");}
+    else { printf("Worker disconnected with error..\n");}
+    
     close(args->sock);
     free(args);
     return 0;
--- tcp_server.c
+++ tcp_server.c
@@ -62,7 +62,7 @@ bool send__file(int sockfd)
     FILE *fp;
     char *end_seq = "--sf--end";
     char fpath[MAX_BUFFER];
-    ssize_t bytes, overall_bytes, filesize = 0;
+    ssize_t overall_bytes, bytes, filesize = 0;
     ssize_t overall_fbytes, fbytes, bytesleft;
     char *metadata, *strfilesize, *data;
     int n_digits_fsize = 0;
@@ -71,19 +71,19 @@ bool send__file(int sockfd)
     if(fgets(fpath, sizeof(fpath), stdin) != NULL)
     {
         size_t len = strlen(fpath);
-        if(len > MAX_BUFFER)
+        if(len > MAX_BUFFER){
             return false;
-
-        if(fpath[len -1] == '\n')
-            fpath[len - 1] = '\0';
+        }
+        if(fpath[len -1] == '\n'){
+            fpath[len -1] = '\0';
+        }
     }
 
-    if ((fp = fopen(fpath, "rb")) == NULL){
-        fprintf(stderr, "Error fopen() rb\n");
+    if((fp = fopen(fpath, "rb")) == NULL){
+        fprintf(stderr, "fopen() error rb\n");
         return false;
     }
 
-    // get file size
     fseek(fp, 0L, SEEK_END); /* jump to end of file */
     filesize = ftell(fp);    /* current byte of file == filesize */
     rewind(fp);              /* jump to beginning of file */
@@ -91,11 +91,11 @@ bool send__file(int sockfd)
 
     // convert filesize to string
     n_digits_fsize = floor(log10(abs(filesize))) + 1;
-    metadata = malloc(strlen(fpath) +1 + n_digits_fsize);
-    strfilesize = malloc(strlen(metadata) +1);
+    metadata       = malloc(strlen(fpath) +1 + n_digits_fsize);
+    strfilesize    = malloc(strlen(metadata) +1);
 
     if(metadata == NULL || strfilesize == NULL){
-        perror("metadata or strfilesize malloc failed.");
+        perror("metadata or strfilesize malloc error");
         return false;
     }
 
@@ -107,7 +107,7 @@ bool send__file(int sockfd)
     // send file metadata
     printf("Sending file metadata..\n");
     if((bytes = send(sockfd, metadata, strlen(metadata), 0)) == -1){
-        perror("send file metadata failed.");
+        perror("send() metadata error");
         return false;
     }
 
@@ -119,34 +119,33 @@ bool send__file(int sockfd)
     printf("Sending data..\n");    
     data = malloc(filesize); // allocate filesize memory for data
     if(data == NULL){
-        perror("data malloc failed.");
+        perror("malloc() error");
         return false;
     }
 
     while((fbytes = fread(data, sizeof(char), filesize, fp)) > 0)
     {
         overall_fbytes += fbytes;
-        
         if((bytes = send(sockfd, data, fbytes, 0)) == -1){
-            perror("send data failed.");
+            perror("send() error");
             return false;
         }
         memset(data, 0, filesize);
-
         overall_bytes += bytes;
-        bytesleft -= bytes;
+        bytesleft     -= bytes;
     }
 
     // send seq as flag indicating EOF
     printf("Sending end-seq..\n");
     sleep(1); // useless??
     if((bytes = send(sockfd, end_seq, strlen(end_seq), 0)) == -1){
-        perror("send end-seq. failed.");
+        perror("send() end-seq. error");
         return false;
     }
     if(overall_bytes != filesize){
         printf("Data loss: fsize: %ld / sent: %ld / bytes-left: %ld", filesize, overall_bytes, bytesleft);
-    } else {
+    }
+    else {
         printf("Success!\nRead from file: [%ld]\nSent: [%ld]b\n", overall_fbytes, overall_bytes);
     }
     free(data);
@@ -154,25 +153,21 @@ bool send__file(int sockfd)
     return true;
 }
 
-
-
 void *send_handler(void *args) // passing multiple args by pointer to struct
 { 
     thr_args *send_args = args;
     ssize_t bytes = 0;
-
     printf("Send handler %ld start..\n", pthread_self());
-
     while(1)
     {    
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
-
         memset(send_args->send_msg, '\0', sizeof(send_args->send_msg));
         fgets(send_args->send_msg, sizeof(send_args->send_msg), stdin);
 
-        send_args->send_msg[strlen(send_args->send_msg) -1] = '\0';
-
+        if(send_args->send_msg[strlen(send_args->send_msg) -1] == '\n'){
+            send_args->send_msg[strlen(send_args->send_msg) -1] = '\0';
+        }
         if(send_args->send_msg[0] == '\0'){
             printf("Enter msg..\n");
         }
@@ -181,33 +176,40 @@ void *send_handler(void *args) // passing multiple args by pointer to struct
             exit(0);
         }
         else if(!strncmp(send_args->send_msg,"-sf",3)){
-            printf("Sending file..\n");
-            // send '-sf' seq as flag indicating incoming file
+            // '-sf' seq as flag indicating incoming file
             send(send_args->sock, send_args->send_msg, strlen(send_args->send_msg), 0);
-            
-            if(!send__file(send_args->sock))
-                printf("Error sending file.\n");
+            printf("Sending file..\n");
+            if(!send__file(send_args->sock)){ perror("send_handler() error"); }
         }
         else {
-            // check if client is connected
-            if(recv(send_args->sock,NULL,1, MSG_PEEK | MSG_DONTWAIT) == 0)
-            {
+            /* check if client is connected */
+            if(recv(send_args->sock,NULL,1, MSG_PEEK | MSG_DONTWAIT) == 0){
                 printf("%d-%02d-%02d %02d:%02d:%02d: Client disconnected\n",
-                    tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec
+                    tm.tm_year + 1900,
+                    tm.tm_mon + 1,
+                    tm.tm_mday,
+                    tm.tm_hour,
+                    tm.tm_min,
+                    tm.tm_sec
                 );
                 break;
             }
-
             bytes = send(send_args->sock, send_args->send_msg, strlen(send_args->send_msg), 0);
             if(bytes > 0){
                 printf("%d-%02d-%02d %02d:%02d:%02d: Sent to: %s:%d \tlen: %ld\n",
-                    tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min,
-                    tm.tm_sec, send_args->converted_addr, send_args->port, bytes
+                    tm.tm_year + 1900,
+                    tm.tm_mon + 1,
+                    tm.tm_mday,
+                    tm.tm_hour,
+                    tm.tm_min,
+                    tm.tm_sec,
+                    send_args->converted_addr,
+                    send_args->port,
+                    bytes
                 );  
             }
         }
     }
-
     close(send_args->sock);
     free(send_args);
 
@@ -219,30 +221,33 @@ void *recv_handler(void *args)
 {
     thr_args *recv_args = args;
     ssize_t bytes = 0;
-
     printf("Recv handler %ld start..\n", pthread_self());
-
     while(1)
     {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
-
         bytes = recv(recv_args->sock, recv_args->buffer, sizeof(recv_args->buffer), 0);
-
         if(bytes > 0){
             printf("%d-%02d-%02d %02d:%02d:%02d: Client [%s:%d] : %s\n",
-                tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
-                tm.tm_min, tm.tm_sec, recv_args->converted_addr, recv_args->port, recv_args->buffer
+                tm.tm_year + 1900,
+                tm.tm_mon + 1,
+                tm.tm_mday,
+                tm.tm_hour,
+                tm.tm_min,
+                tm.tm_sec,
+                recv_args->converted_addr,
+                recv_args->port,
+                recv_args->buffer
             );
             memset(recv_args->buffer, 0, MAX_BUFFER);
         }
-
         else if(bytes == 0){
             printf("No data from [%s:%d]\n",recv_args->converted_addr, recv_args->port);
             break;
         }
-
-        else perror("recv");
+        else {
+            perror("recv_handler() error");
+        }
     }
     printf("Recv handler %ld exited normaly..\n", pthread_self());
     pthread_exit(0);
@@ -255,65 +260,56 @@ bool isValidIp(const char *ip){
 }
 
 int main(int argc, const char **argv) {
-    struct sockaddr_in client_addr;           // client addr
-    int server_fd;                            
-    int opt = 1;                              // sock option
-    int addrlen = sizeof(client_addr);        // length of client addr
-    char buffer[1024];                        // recv from clients
-    int port = 0;
-    int i;
+    struct sockaddr_in client_addr;    /* client addr           */
+    char buffer[MAX_BUFFER];           /* recv from clients     */
+    int addrlen = sizeof(client_addr); /* length of client addr */
+    int opt = 1;                       /* sock option           */
+    int i, server_fd, port = 0;                            
 
     memset(&client_addr, '\0', sizeof(client_addr));
     memset(buffer, '\0', sizeof(buffer));
 
-    if(argc < 3)
-    {
-        printf("Enter ip/port.\nUsage: ./client <ip_addr> <port>\n");
+    if(argc < 3){
+        printf("Enter ip/port. Usage: ./server <ip_addr> <port>\n");
         return 1;
     }
-
     if(!isValidIp(argv[1])){
         printf("Invalid ip.\n");
         return 1;
     }
-
     for(i=0; i<strlen(argv[2]); i++)
     {
-        if(!isdigit(argv[2][i])) {
+        if(!isdigit(argv[2][i])){
             printf("Invalid port.\n");
             return 1;
         }
     }
-
     if((port = atoi(argv[2])) > 65535 && port < 0){
         printf("Port number not in range.\n");
         return 1;
     }
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {   // in socket() function, 0 is default protocol [TCP]
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){   // in socket(), 0 is default protocol [TCP]
+        perror("socket() error");
+        return 1;
     }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))){
+        perror("setsockopt() error");
+        return 1;
     }
 
     client_addr.sin_family      = AF_INET;       
     client_addr.sin_addr.s_addr = INADDR_ANY;    // bind to all interfaces
     client_addr.sin_port        = htons(port); 
 
-    if (bind(server_fd, (struct sockaddr *)&client_addr, sizeof(client_addr))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    if(bind(server_fd, (struct sockaddr *)&client_addr, sizeof(client_addr))<0) {
+        perror("bind() error");
+        return 1;
     }
-
     // THREAD_NUM queue limit for handlers
     printf("Listening for incomming connections on port %d..\n", port);
-    if (listen(server_fd, THRNUM) < 0) {                
-        perror("listen");
-        exit(EXIT_FAILURE);
+    if(listen(server_fd, THRNUM) < 0){                
+        perror("listen() error");
+        return 1;
     }
 
     pthread_t send_handler_id[THRNUM];
@@ -321,49 +317,39 @@ int main(int argc, const char **argv) {
 
     for(i=1;i<=THRNUM;i++)
     {
-
         thr_args *args = malloc(sizeof *args);       
-        char clientAddr [16] = "";     // displaying ips
+        char clientAddr [16] = ""; // displaying ips
 
-        if ((args->sock = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
+        if((args->sock = accept(
+            server_fd,
+            (struct sockaddr *)&client_addr,
+            (socklen_t*)&addrlen)) < 0)
+        { perror("accept() error"); }
 
         // convert addr from network format to text format
         inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, clientAddr, sizeof(clientAddr));
         printf("Client [%s:%d] connected!\n",clientAddr, client_addr.sin_port);
 
-        args->port = client_addr.sin_port;   // copy client port -> handler parameter
-
-        // handler param
-        strncpy(args->converted_addr, clientAddr, strlen(clientAddr));
+        args->port = client_addr.sin_port; /* copy client port -> handler param */
+        strncpy(args->converted_addr, clientAddr, strlen(clientAddr)); /* handler param */
 
         if(pthread_create(&send_handler_id[i], NULL, send_handler, args) != 0){ 
             close(args->sock);
-            printf("Error creating [%ld] send handler..",send_handler_id[i]);
+            printf("error creating [%ld] send handler..",send_handler_id[i]);
         }
-
         if(pthread_create(&recv_handler_id[i], NULL, recv_handler, args) != 0){
             close(args->sock);
-            printf("Error creating [%ld] recv handler...",recv_handler_id[i]);
+            printf("error creating [%ld] recv handler...",recv_handler_id[i]);
         }
-
-        printf("Created %d threads..\n",(i * 2));
+        printf("created %d threads..\n",(i * 2));
     }
-
-
     for(i=1;i<=THRNUM;i++)
     {
         if(pthread_join(send_handler_id[i],NULL) == 0 
-         && pthread_join(recv_handler_id[i],NULL) == 0){
-            printf("Worker %d disconnected normaly..\n",i);
-        }
-        else {
-            printf("Worker %d disconnected with error..\n",i);
-        }
+        && pthread_join(recv_handler_id[i],NULL) == 0)
+        { printf("worker %d disconnected normaly..\n",i); }
+        else { printf("worker %d disconnected with error..\n",i); }
     }
-
     close(server_fd);
     return 0;
 }
